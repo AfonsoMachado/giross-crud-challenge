2d2c3321af744dd7606d833c1ca3539781457172
Afonso Machado <afonsosmachado@gmail.com>
2022-11-08 16:15:42 -0300

    user entity

--- giross-crud-server/src/users/entities/user.entity.ts
+++ giross-crud-server/src/users/entities/user.entity.ts
@@ -1,1 +1,43 @@
-export class User {}
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+
+@Entity()
+export class User {
+  @ApiProperty()
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ApiProperty()
+  @Column()
+  name: string;
+
+  @ApiProperty()
+  @Column()
+  email: string;
+
+  @ApiProperty()
+  @Column()
+  password: string;
+
+  @ApiProperty()
+  @Column()
+  phone: string;
+
+  @ApiProperty()
+  @Column()
+  gender: string;
+
+  @ApiProperty()
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @ApiProperty()
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
