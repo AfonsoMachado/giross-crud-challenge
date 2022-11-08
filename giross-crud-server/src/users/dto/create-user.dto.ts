ce0fd2cc323bc15915ed22deaadf38c5dd504a45
Afonso Machado <afonsosmachado@gmail.com>
2022-11-08 16:25:35 -0300

    add user dto

--- giross-crud-server/src/users/dto/create-user.dto.ts
+++ giross-crud-server/src/users/dto/create-user.dto.ts
@@ -1,1 +1,23 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
+export class CreateUserDto {
+  @ApiProperty()
+  @IsNotEmpty({ message: 'É necessário informar o nome.' })
+  name: string;
+
+  @ApiProperty()
+  @IsEmail({}, { message: 'É Necessário informar um e-mail válido' })
+  @IsNotEmpty({ message: 'É necessário informar o e-mail.' })
+  email: string;
+
+  @ApiProperty()
+  @IsNotEmpty({ message: 'É necessário informar a senha.' })
+  password: string;
+
+  @ApiProperty()
+  phone: string;
+
+  @ApiProperty()
+  gender: string;
+}
