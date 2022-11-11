c983ecfcc4db645b76268776b9602ac2a871d98a
Afonso Machado <afonsosmachado@gmail.com>
2022-11-11 16:14:14 -0300

    add routes

--- giross-crud-web/src/app/app-routing.module.ts
+++ giross-crud-web/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomerCrudComponent } from './views/customer-crud/customer-crud.component';
+import { HomeComponent } from './views/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'customers',
+    component: CustomerCrudComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
