b617d3b01be88922009a2e6f982eb67cc53ba26f
Afonso Machado <afonsosmachado@gmail.com>
2022-11-13 23:03:32 -0300

    Update app.component.spec.ts

--- giross-crud-web/src/app/app.component.spec.ts
+++ giross-crud-web/src/app/app.component.spec.ts
@@ -19,17 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'giross-crud-web'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('giross-crud-web');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('giross-crud-web app is running!');
-  });
 });
